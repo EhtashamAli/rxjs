f230c9b2399fc8f67836c2e1f5b8f90a8fba0d61
Ehtasham ali <ehtasham.ali1979@gmail.com>
2020-11-19 13:43:06 -0800

    component tests

--- src/App.test.js
+++ src/App.test.js
@@ -8,3 +8,22 @@ test('renders dashboard heading', () => {
   const linkElement = screen.getByText(/Dashboard/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+test('renders temperature monitor', () => {
+  render(<Provider store={store}><App /></Provider>);
+  const linkElement = screen.getByText(/Temperature/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders air pressure monitor', () => {
+  render(<Provider store={store}><App /></Provider>);
+  const linkElement = screen.getByText(/Air Pressure/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders humidity monitor', () => {
+  render(<Provider store={store}><App /></Provider>);
+  const linkElement = screen.getByText(/Humidity/i);
+  expect(linkElement).toBeInTheDocument();
+});
